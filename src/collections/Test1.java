1cd89603e4a3f1f945d914200f4c27143da0da14
Elvin Abaev <elvinabaev@gmail.com>
2023-09-02 14:22:07 +0300

    Meeting with Collections Framework. LinkedList.

---
+++ src/collections/Test1.java
@@ -0,0 +1,29 @@
+package collections;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Test1 {
+    public static void main(String[] args) {
+        List<Integer> myList = new LinkedList<>();
+        List<Integer> arrayList = new ArrayList<>();
+
+        measureTime(myList);
+        measureTime(arrayList);
+
+
+    }
+
+    private static void measureTime(List<Integer> list) {
+        long start = System.currentTimeMillis();
+
+        for (int i = 0; i < 1000000; i++) {
+            list.add(i);
+        }
+
+        long end = System.currentTimeMillis();
+
+        System.out.println(end - start);
+    }
+}
